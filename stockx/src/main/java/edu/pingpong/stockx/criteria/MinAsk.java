b0b940dbfb2e2150bca5099536fc741731fe7d1a
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-02-21 16:42:21 +0100

    feat: :sparkles: minAsk

--- stockx/src/main/java/edu/pingpong/stockx/criteria/MinAsk.java
+++ stockx/src/main/java/edu/pingpong/stockx/criteria/MinAsk.java
@@ -1,10 +1,27 @@
-/**package edu.pingpong.stockx.criteria;
+package edu.pingpong.stockx.criteria;
 
-public class MinAsk implements Criteria{
-    public MinAsk(){}
+import edu.pingpong.stockx.item.Item;
+import edu.pingpong.stockx.item.Offer;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class MinAsk implements Criteria {
+    private List<Offer> minAsk = new ArrayList<>();
+    Criteria asks = new Asks();
+
+    public MinAsk() {
+    }
+
     @Override
-public List<Offer> checkCriteria(Item cosa){
-    
+
+    public List<Offer> checkCriteria(Item sneaker) {
+        Offer minimumAsk = asks.checkCriteria(sneaker).stream().filter(a -> a != null)
+                .min(Comparator.comparing(a -> a.value())).orElse(null);
+        minAsk.add(minimumAsk);
+
+        return minAsk;
+    }
+
 }
-    
-}*/
