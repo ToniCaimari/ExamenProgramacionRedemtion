285a7821a5611e1ef78a9857e26fdd803a3fa870
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-02-21 19:36:25 +0100

    feature: Size.java

--- stockx/src/main/java/edu/pingpong/stockx/criteria/Size.java
+++ stockx/src/main/java/edu/pingpong/stockx/criteria/Size.java
@@ -1,17 +1,26 @@
-/**package edu.pingpong.stockx.criteria;
+package edu.pingpong.stockx.criteria;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import edu.pingpong.stockx.item.Item;
+import edu.pingpong.stockx.item.Offer;
+import java.util.ArrayList;
 
 public class Size implements Criteria {
-    public Size() {
+    private List<Offer> sameSize = new ArrayList<>();
+    private String size;
+    Criteria sizes = new Size(size);
+
+    public Size(String size) {
+        this.size = size;
     };
 
-
     @Override
-    public List<Offer> checkCriteria(Item cosa) {
-        // TODO Auto-generated method stub
-        return null;
+    public List<Offer> checkCriteria(Item sneaker) {
+        List<Offer> sameSize = sneaker.offers().stream().filter(a -> a.size().equals(this.size))
+                .collect(Collectors.toList());
+        return sameSize;
     }
-    
+
 }
-*/
