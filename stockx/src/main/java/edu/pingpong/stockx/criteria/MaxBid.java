c90860fa1f460d7ba9ef23765e729d7cd3a11e13
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-02-21 16:36:37 +0100

    feat: :sparkles: MaxBid
    maxBid finds the max bid in Bids

--- stockx/src/main/java/edu/pingpong/stockx/criteria/MaxBid.java
+++ stockx/src/main/java/edu/pingpong/stockx/criteria/MaxBid.java
@@ -1,20 +1,25 @@
 package edu.pingpong.stockx.criteria;
 
-import edu.pingpong.stockx.item.Bid;
 import edu.pingpong.stockx.item.Item;
 import edu.pingpong.stockx.item.Offer;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MaxBid implements Criteria {
-    private static final List<Offer> maxBid = null;
-    Bids bids;
+    private static List<Offer> maxBid = new ArrayList<>();
+    Criteria bids = new Bids();
 
     public MaxBid() {
     }
 
     @Override
     public List<Offer> checkCriteria(Item sneaker) {
+        Offer maximumBid = bids.checkCriteria(sneaker).stream().filter(a -> a != null)
+                .max(Comparator.comparing(a -> a.value())).orElse(null);
+        maxBid.add(maximumBid);
+
         return maxBid;
     }
 
