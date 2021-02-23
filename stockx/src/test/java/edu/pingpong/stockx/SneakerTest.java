6e61d83db825ea67a82f7d2b3a9049318bbc0cc3
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-02-23 04:24:55 +0100

    test: :white_check_mark: Sneaker Tests

--- stockx/src/test/java/edu/pingpong/stockx/SneakerTest.java
+++ stockx/src/test/java/edu/pingpong/stockx/SneakerTest.java
@@ -1,24 +1,38 @@
 package edu.pingpong.stockx;
 
-import edu.pingpong.stockx.item.Offer;
-import edu.pingpong.stockx.item.Sneaker;
-
-import static org.junit.Assert.assertArrayEquals;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.pingpong.stockx.item.Ask;
+import edu.pingpong.stockx.item.Bid;
+import edu.pingpong.stockx.item.Item;
+import edu.pingpong.stockx.item.Sneaker;
+
 public class SneakerTest {
-  
-    
-    /**
-    *  No hay casos test y soy consciente de la gravedad de ello...
-    *  He perdido muchísimo tiempo con el bloqueo del inicio pero 
-    * no excusa la falta de preparación
-    */
-    
+
+    @Test
+    public void toStringTest() {
+        Item sneaker = new Sneaker("Estilo", "Nombre");
+        assertEquals("\n\t" + "Nombre" + "\n\t\t" + "Estilo", sneaker.toString());
     }
+
+    @Test
+    public void addBidTest() {
+        Item sneaker = new Sneaker("Estilo", "Nombre");
+        sneaker.add(new Bid("9.5", 50));
+        sneaker.add(new Bid("5", 100));
+        assertEquals(2, sneaker.offers().size());
+    }
+
+    @Test
+    public void addBidAskTest() {
+        Item sneaker = new Sneaker("Estilo", "Nombre");
+        sneaker.add(new Bid("9.5", 50));
+        sneaker.add(new Bid("5", 10));
+        sneaker.add(new Ask("7", 200));
+        sneaker.add(new Ask("8.5", 30));
+        assertEquals(4, sneaker.offers().size());
+    }
+}
