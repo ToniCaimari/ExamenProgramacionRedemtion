18337efb7284c54e9720f60ff4404e55bb05ba85
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-02-23 01:57:21 +0100

    fix: :bug: Size eliminación ArrayList sobrantes
    He eliminado la inicialización de unas arraylist que no dejaban trabajar al codigo

--- stockx/src/main/java/edu/pingpong/stockx/criteria/Size.java
+++ stockx/src/main/java/edu/pingpong/stockx/criteria/Size.java
@@ -5,12 +5,10 @@ import java.util.stream.Collectors;
 
 import edu.pingpong.stockx.item.Item;
 import edu.pingpong.stockx.item.Offer;
-import java.util.ArrayList;
 
 public class Size implements Criteria {
-    private List<Offer> sameSize = new ArrayList<>();
+
     private String size;
-    Criteria sizes = new Size(size);
 
     public Size(String size) {
         this.size = size;
