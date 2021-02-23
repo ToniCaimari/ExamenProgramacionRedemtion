e699d2ce1308fe58bc642a63ba1ac79ed2c9680d
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-02-23 04:23:22 +0100

    feat: :construction: unfinished Max try

--- stockx/src/main/java/edu/pingpong/stockx/criteria/Max.java
+++ stockx/src/main/java/edu/pingpong/stockx/criteria/Max.java
@@ -1,10 +1,34 @@
-/**package edu.pingpong.stockx.criteria;
+// package edu.pingpong.stockx.criteria;
 
-public class Max implements Criteria{
-    public Max(){}
-    @Override
-public List<Offer> checkCriteria(Item cosa){
-    
-}
-    
-}*/
+// import java.util.ArrayList;
+// import java.util.Comparator;
+// import java.util.List;
+// import java.util.stream.Collectors;
+
+// import edu.pingpong.stockx.item.Item;
+// import edu.pingpong.stockx.item.Offer;
+
+// public class Max implements Criteria {
+// private Criteria criteria;
+// private Criteria otherCriteria;
+
+// public Max(Criteria criteria, Criteria otherCriteria) {
+// this.criteria = criteria;
+// this.otherCriteria = otherCriteria;
+// }
+
+// @Override
+// public List<Offer> checkCriteria(Item sneaker) {
+// List<Offer> firstCriteria = criteria.checkCriteria(sneaker);
+// List<Offer> secondCriteria = otherCriteria.checkCriteria(sneaker);
+// List<Offer> andCriteria = new ArrayList<Offer>();
+
+// andCriteria = firstCriteria.stream()
+// .filter(x -> (secondCriteria.stream().max(Comparator.
+// comparing(Offer::value))))
+// .collect(Collectors.toList());
+// return andCriteria;
+// }
+
+// }
+// Necesario usar el compareTo, no soy capaz de momento
