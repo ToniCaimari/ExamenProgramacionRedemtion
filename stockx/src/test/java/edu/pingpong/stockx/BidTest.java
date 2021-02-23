5741228bba14bd5157b14453d9e9c4e0bbcebe23
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-02-23 04:30:08 +0100

    Bid Test

---
+++ stockx/src/test/java/edu/pingpong/stockx/BidTest.java
@@ -0,0 +1,16 @@
+package edu.pingpong.stockx;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import edu.pingpong.stockx.item.Bid;
+
+public class BidTest {
+
+    @Test
+    public void toStringTest() {
+        Bid bid = new Bid("Talla", 200);
+        assertEquals("\n\t\t" + "Size" + "\t\t" + "200", bid.toString());
+    }
+}
